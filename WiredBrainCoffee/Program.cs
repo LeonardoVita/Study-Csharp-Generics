37aeac374649b6222b2724ea2e0c940f69de39bb
LeonardoVita <leonardo.vita0606@gmail.com>
2022-08-21 23:40:44 -0300

    initial Wired coffee

---
+++ WiredBrainCoffee/Program.cs
@@ -0,0 +1,19 @@
+﻿using WiredBrainCoffee.Entities;
+using WiredBrainCoffee.Repositories;
+
+namespace WiredBrainCoffee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            employeeRepository.Add(new Employee { FirstName= "Julia"});
+            employeeRepository.Add(new Employee { FirstName= "Anna"});
+            employeeRepository.Add(new Employee { FirstName= "Thomas"});
+            employeeRepository.save();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WiredBrainCoffee/Repositories/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using WiredBrainCoffee.Entities;
+
+namespace WiredBrainCoffee.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly List<Employee> _employees = new List<Employee>();
+
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        public void save()
+        {
+            foreach (var employee in _employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
