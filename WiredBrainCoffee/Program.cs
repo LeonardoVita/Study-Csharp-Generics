6bef24b95d3c3af27d6d26582e7c9867ccd2b6f4
LeonardoVita <leonardo.vita0606@gmail.com>
2022-08-28 18:47:11 -0300

    Applies Dependency inversion with IRepository

--- WiredBrainCoffee/Program.cs
+++ WiredBrainCoffee/Program.cs
@@ -1,4 +1,5 @@
-﻿using WiredBrainCoffee.Entities;
+﻿using WiredBrainCoffee.Data;
+using WiredBrainCoffee.Entities;
 using WiredBrainCoffee.Repositories;
 
 namespace WiredBrainCoffee
@@ -7,31 +8,36 @@ namespace WiredBrainCoffee
     {
         static void Main(string[] args)
         {
-            var employeeRepository = new ListRepository<Employee>();
+            var employeeRepository = new SqlRepository<Employee>(new StorageAppDbContext());
             AddEmployees(employeeRepository);
             GetEmployeeById(employeeRepository);
 
             var OrganizationRepository = new ListRepository<Organization>();
             AddOrganizations(OrganizationRepository);
+            GetOrganizationById(OrganizationRepository);
 
             Console.ReadLine();
         }
 
-        private static void GetEmployeeById(ListRepository<Employee> employeeRepository)
+        private static void GetOrganizationById(ListRepository<Organization> organizationRepository)
+        {
+            var organization = organizationRepository.GetById(1);
+            Console.WriteLine($"Organization with Id 0: {organization.Name}");
+        }
+
+        private static void GetEmployeeById(IRepository<Employee> employeeRepository)
         {
             var employee = employeeRepository.GetById(2);
             Console.WriteLine($"Employee with Id 2: {employee.FirstName}");
         }
-
-        private static void AddEmployees(ListRepository<Employee> employeeRepository)
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
         {
             employeeRepository.Add(new Employee { FirstName = "Julia" });
             employeeRepository.Add(new Employee { FirstName = "Anna" });
             employeeRepository.Add(new Employee { FirstName = "Thomas" });
             employeeRepository.save();
         }
-
-        private static void AddOrganizations(ListRepository<Organization> OrganizationRepository)
+        private static void AddOrganizations(IRepository<Organization> OrganizationRepository)
         {
             OrganizationRepository.Add(new Organization { Name = "Pluralsight" });
             OrganizationRepository.Add(new Organization { Name = "GloboSat" });
---
+++ WiredBrainCoffee/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using WiredBrainCoffee.Entities;
+
+namespace WiredBrainCoffee.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        void Add(T item);
+        T GetById(int id);
+        void Remove(T item);
+        void save();
+    }
+}
